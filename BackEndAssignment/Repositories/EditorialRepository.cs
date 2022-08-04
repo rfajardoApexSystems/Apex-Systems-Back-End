20d0b1d08e82ceb47b1ae735fd02294c3708c46a
@aceron <jceron@apexsystems.com>
2022-08-03 19:55:40 -0500

    Adding editorial repository and mock db for list implementation

---
+++ BackEndAssignment/Repositories/EditorialRepository.cs
@@ -0,0 +1,48 @@
+﻿using BackEndAssignment.Interfaces.Repositories;
+using BackEndAssignment.Models;
+
+namespace BackEndAssignment.Repositories
+{
+    /*
+     * TODO:
+     * 1.- Implement Get method to get an editorial by id
+     * 2.- Implement Get method to get editorials by filtering with a function
+     * 3.- Implement Remove method to remove an item from the data source
+     * 4.- Implement Update method to remove an item in the data source
+     * 5.- Implement Add method to add an item to the data source
+     */
+    public class EditorialRepository : IEditorialRepository
+    {
+        private EditorialDb _editorialModels;
+
+        public EditorialRepository()
+        {
+            _editorialModels = new EditorialDb();
+        }
+
+        public void Add(EditorialModel model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public EditorialModel Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<EditorialModel> Get(Func<EditorialModel, bool> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(EditorialModel model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(EditorialModel model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
